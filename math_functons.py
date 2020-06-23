eb4ef9c444f854505632c9b5cee818abc42fdff6
trushithanama <67234326+trushithanama@users.noreply.github.com>
2020-06-23 21:42:39 +0530

    Create math_functons.py

---
+++ math_functons.py
@@ -0,0 +1,15 @@
+math import sin 
+sin_60 = sin(60)
+print("the sine of 30 = ", sin_60)
+
+
+
+math import cos
+cos_pi = cos(pi)
+print("the cos of pi = ", cos_pi)
+
+
+
+math import tan
+tan_90 = tan(90)
+print("the tan of 90 = ", tan_90)
